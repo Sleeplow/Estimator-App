6cd05ca3a015e28329f2e96ffb5bf9c4d08250c8
Sleeplow <Sleeplow@users.noreply.github.com>
2026-04-20 14:20:22 +0000

    deploy: c130f988370c3ad1cfc59c0eb95ef3675af3efae

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Estimator-App/sw.js', { scope: '/Estimator-App/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
